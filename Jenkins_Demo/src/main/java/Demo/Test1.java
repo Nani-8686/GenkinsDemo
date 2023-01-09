1d3cb2a0c730aedd861036dfca88c6b37875d11d
M.SURESH <M.SURESH@LAPTOP-EPP5AGB4>
2023-01-09 16:09:16 +0530

    f

---
+++ Jenkins_Demo/src/main/java/Demo/Test1.java
@@ -0,0 +1,17 @@
+package Demo;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+
+public class Test1 {
+//testng
+//java Selenium
+//maven sure dependencies
+//usage plugins
+	@Test
+	public void m1()
+	{
+	   Reporter.log("hello");
+		
+	}
+}
---
+++ Jenkins_Demo/src/main/java/Demo/Test2.java
@@ -0,0 +1,14 @@
+package Demo;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+
+public class Test2 {
+	@Test
+	public void m2()
+	{
+	   Reporter.log("hii");
+		
+	}
+
+}
